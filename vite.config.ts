36a950e76f0035ea545a2c1f8780b97d0aa5d6a9
natanngl03 <roy.natanaelngl03@gmail.com>
2026-09-03 04:05:37 +0700

    add sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 /// <reference types="vite-react-ssg" />
 import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import sitemap from "vite-plugin-sitemap";
 import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
-   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+   plugins: [
+      react(),
+      babel({ presets: [reactCompilerPreset()] }),
+      sitemap({
+         hostname: "https://webtangsel.com",
+         outDir: "docs",
+      }),
+   ],
    build: {
       outDir: "docs",
    },
